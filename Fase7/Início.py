8488407589abea3688e00492db7ab30c9e9d831d
Guilherme Hermanowski <126830998+guilherme-hermanowski@users.noreply.github.com>
2025-11-23 21:07:54 -0300

    Delete Fase7/Início.py

--- Fase7/Início.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-st.set_page_config(page_title='Análise Agrícola',page_icon='🌱',layout='wide')
-st.title('FarmTech Solutions 🌱')
-st.write('Bem-vindo  ao  aplicativo  de  análise  de  dados agrícolas.')
-st.markdown("""A FarmTech Solutions é uma startup que vem inovando no ramo do agronegócio e trazendo soluções inovadoras para o ramo, como a criação de modelos preditivos, análises de safras e muito mais!""")
-st.markdown(""" Este aplicativo permite explorar um dataset simulado de produção agrícola, realizar análises  exploratórias  e  aplicar  modelos preditivos. Utilize o menu à esquerda para navegar entre as páginas.""")
